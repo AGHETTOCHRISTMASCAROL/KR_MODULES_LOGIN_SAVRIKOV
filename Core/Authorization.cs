4e13504d952eaa25db3346e8f5eba6dabcfb4014
ione skywalker <ivansavrikow@gmail.com>
2023-02-20 18:45:51 +0700

    yeap

---
+++ Core/Authorization.cs
@@ -0,0 +1,28 @@
+﻿using _20101_Savrikov_authorization.Model;
+using Microsoft.SqlServer.Server;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace _20101_Savrikov_authorization.Core
+{
+    public class ForAuthorization
+    {
+        public void GetAuthorization(string login, string password)
+        {
+            UsersRolesBaseEntities db = UsersRolesBaseEntities.GetContext();
+
+            if (db.User.Any(u => u.login == login && u.password == password))
+            {
+                var user = db.User.Where(u => u.login == login && u.password == password).FirstOrDefault();
+                int idRole = user.Role.idRole;
+                MessageBox.Show($"Добро пожаловать! Ваша роль: {user.Role.title}");
+            } 
+            else
+                MessageBox.Show("Неверно введены логин или пароль!");
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using _20101_Savrikov_authorization.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _20101_Savrikov_authorization
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            ForAuthorization authorization = new ForAuthorization();
+
+            string login = tbLogin.Text;
+            string password = pbPassword.Password;
+
+            authorization.GetAuthorization(login, password);
+        }
+    }
+}
